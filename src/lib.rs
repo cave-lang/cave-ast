70dffae6c6c544c219f816ff53d5ede7566e1335
Xuanmizhen <xuanmizhen@icloud.com>
2025-07-15 03:55:43 +0000

    Install requirements

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+pub enum Symbol {
+    // TODO: Finish the definition
+}
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
